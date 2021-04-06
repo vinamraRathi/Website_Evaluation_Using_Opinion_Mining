a32cafd448b0d7900bb39fec54b77f59b31d5a8f
vinamraRathi <vinamrarathi771@gmail.com>
2021-04-06 23:53:24 +0530

    User Password Model Done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UserSignUpView
+from .views import UserSignUpView, UserEditView, PasswordsChangeView
+from . import views
 
 urlpatterns = [
     path('signup/', UserSignUpView.as_view(), name="signup"),
+    path('edit_profile/', UserEditView.as_view(), name="Edit-Profile"),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    path('password_updated/', views.password_success, name='password_success'),
 ]
